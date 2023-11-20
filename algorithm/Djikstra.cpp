25d5d0f5f671158f807ebb2ac9a59b57515c4487
Kamil Uchwat <92688578+Hirocco@users.noreply.github.com>
2023-11-20 18:43:04 +0100

    Add files via upload

--- algorithm/Djikstra.cpp
+++ algorithm/Djikstra.cpp
@@ -3,3 +3,99 @@
 //
 
 #include "Djikstra.h"
+
+/*
+ * TODO:
+ * Algorytm musi jakoś respektować ściany (wallArr)
+ * Algorytm musi działać w mniej rozjebany sposób.
+ * Przeczyścić kod by sie nie powtarzał
+ */
+
+std::vector<std::vector<Coordinate>> Djikstra::shortestPath() {
+
+    // Definiujemy wewnętrzny wektor z wartościami 'nieskończoność' - nie wiemy jaki jest dystans na początku do każdego tile'a.
+    std::vector<int> innerVector(mapSizeY, std::numeric_limits<int>::max());
+
+    //Definiujemy wektor 2D za pomocą wektora wewntętrznego. Potrzebny jest nam on do kontrolowania kosztu dostania się do każdego z kwadracików
+    std::vector<std::vector<int>> distance(mapSizeX, innerVector);
+
+    /*
+     * This line declares a priority queue named `pq` that stores pairs of an integer (representing distance) and a `Coordinate` struct.
+     * It uses a `std::vector` as the underlying container and custom comparison logic provided by the `CompareDistance`
+     * comparator class to prioritize elements based on their distances.
+     */
+    std::priority_queue<std::pair<int, Coordinate>, std::vector<std::pair<int,Coordinate>>, CompareDistance> pq;
+
+    //wypelnimy danymi -1,-1 ktore potem zmienimy na odpowiednie
+    std::vector<Coordinate> uninitializedPath(mapSizeY);
+    std::vector<std::vector<Coordinate>> predecessor(mapSizeX,uninitializedPath);
+
+    Coordinate invalidCords = {-1,-1};
+    for (int x = 0; x < mapSizeX; ++x) {
+        for (int y = 0; y < mapSizeY; ++y) {
+            predecessor[x][y] = invalidCords;
+        }
+    }
+    //printf("Predecessor zainicjowany.\n"); dziala
+    startPoint.wage = 0;
+    pq.emplace(startPoint.wage,startPoint);
+
+    //pozycje sa jako 0,50,100,150 etc. w tileMap
+    //wypelniam kolejke wg najblizszych koordynatow
+    while(!pq.empty()){
+        auto current = pq.top().second;
+        pq.pop();
+
+        //Sprawdzamy sasiadow
+        for(int dx = -1;dx<=1;dx++){
+            for(int dy = -1;dy<=1;dy++){
+                //pomijamy aktualny node
+                if(dx ==0 && dy == 0) continue;
+                int newRow = current.row + dx;
+                int newCol = current.col + dy;
+
+                //sprawdzamy czy nowe kolumny i rzedy sa w zasiegu mapy
+                if(!wallArr.empty())
+                {
+                    for(int i=0;i<wallArr.size();i++) {
+                        if (newRow < mapSizeX && newCol < mapSizeY && newCol >= 0 && newRow >= 0 && newRow != wallArr[i].row && newCol != wallArr[i].col) {
+                            //zwiekszamy dystanst o 1 poniewaz poczynilismy jeden krok w jakas strone
+                            int newDistance = current.wage + 1;
+
+                            if (newDistance < distance[newRow][newCol]) {
+                                //distance update
+                                distance[newRow][newCol] = newDistance;
+
+                                //update predecessora
+                                predecessor[newRow][newCol] = current;
+                                //dodajemy sasiada
+                                pq.emplace(newDistance, Coordinate{newRow, newCol, newDistance});
+                            }
+                        }
+                    }
+                }else{
+                    if (newRow < mapSizeX && newCol < mapSizeY && newCol >= 0 && newRow >= 0 ) {
+                        //zwiekszamy dystanst o 1 poniewaz poczynilismy jeden krok w jakas strone
+                        int newDistance = current.wage + 1;
+
+                        if (newDistance < distance[newRow][newCol]) {
+                            //distance update
+                            distance[newRow][newCol] = newDistance;
+
+                            //update predecessora
+                            predecessor[newRow][newCol] = current;
+                            //dodajemy sasiada
+                            pq.emplace(newDistance, Coordinate{newRow, newCol, newDistance});
+                        }
+                    }
+                }
+
+            }
+        }
+
+    }
+
+    return predecessor;
+}
+
+
--- algorithm/Djikstra.h
+++ algorithm/Djikstra.h
@@ -6,52 +6,27 @@
 #define PROJECT_CPP_DJIKSTRA_H
 
 #include "../constants.h"
-
-Coordinate startPoint;
-Coordinate finishPoint;
-Coordinate newTileCoordinate;
-std::vector<Coordinate> wallArr;
+#include "../grid/Grid.h"
 
 class Grid;
-
 
 class Djikstra {
     friend class Grid;
 private:
-    std::vector<visitedNode> coordsOfVisitedTiles;
+    std::vector<std::vector<Coordinate>> shortestPath();
+    /*
+    komparator do priorytezowania mniejszych dystansów.
+    jest to funktor (obiekt który można wywołać jak funkcje) - wiecej w notatki.txt
+    overload ()
+    */
+    struct CompareDistance{
+        bool operator()(const std::pair<int, Coordinate>& a, const std::pair<int,Coordinate>& b){
+            return a.first> b.first;
+        }
+    }typedef CompareDistance;
 
 };
 
-
 #endif //PROJECT_CPP_DJIKSTRA_H
-/*
- *  Co ma robić ten algorytm :
- *  Algorytm Dijkstry jest używany do znalezienia najkrótszej ścieżki w grafie ważonym od jednego punktu (źródła) do wszystkich pozostałych punktów w grafie.
- *  W naszym przypadku będziemy go używać do znalezienia najkrótszej ścieżki od startPoint do finishPoint w naszej planszy.
- *
- *  1. Inicjalizacja:
- *     - Utwórz zbiór odwiedzonych punktów i zainicjuj go jako pusty.
- *     - Utwórz wektor wagi, który będzie przechowywał odległości od startPoint do wszystkich innych punktów na planszy.
- *     - Inicjalizuj wagi punktów jako "nieskończoność" (dla punktów odległych od startPoint) i 0 dla samego startPoint.
- *      Ten punkt został zrealizowany za pomocąstruktury visitedNode.
- *
- *  2. Iteracyjne odwiedzanie punktów:
- *     - Iteruj, dopóki istnieją nieodwiedzone punkty:
- *       - Wybierz punkt o najmniejszej wadze z wierzchołków nieodwiedzonych.
- *       - Oznacz ten punkt jako odwiedzony.
- *
- *  3. Aktualizacja wag:
- *     - Dla każdego sąsiedniego punktu, który jeszcze nie został odwiedzony i jest osiągalny z aktualnego punktu:
- *       - Oblicz nową wagę dla tego punktu jako suma wagi aktualnego punktu i wagi krawędzi między nimi.
- *       - Jeśli nowa waga jest mniejsza od poprzedniej wagi dla tego punktu, zaktualizuj wagę punktu.
- *
- *  4. Powrót do 2:
- *     - Powtarzaj kroki 2 i 3, aż wszystkie punkty zostaną odwiedzone.
- *
- *  5. Wyznaczenie najkrótszej ścieżki:
- *     - Rozpocznij od finishPoint i przejdź do startPoint, śledząc ścieżkę na podstawie wag punktów.
- *     - Ścieżka będzie odwrócona, więc odwróć ją, aby uzyskać kolejność od startPoint do finishPoint.
- *
- *  6. Zakończenie:
- *     - Wynikiem algorytmu będzie najkrótsza ścieżka od startPoint do finishPoint oraz jej długość.
- */
+
+
